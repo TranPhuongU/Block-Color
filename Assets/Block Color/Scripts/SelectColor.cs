ca02929f5f3c9bde47dd155b95a1bffb47cbb859
TranPhuongU <fuong9103@gmail.com>
2026-01-19 21:19:03 +0700

    d
    d

---
+++ Assets/Block Color/Scripts/SelectColor.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class SelectColor : MonoBehaviour, IPointerClickHandler
+{
+    public PieceColor colorSelect;
+    public GameObject activeFrame;
+    public GameObject arrow;
+
+
+    private void Start()
+    {
+        if(colorSelect == PieceColor.Blue)
+        {
+            GameManager.instance.currentSelect = this;
+        }
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(GameManager.instance.GetGameState() != GameState.Gameplay)
+            return;
+
+        SoundManager.instance.PlayButtonSound();
+
+        if(GameManager.instance.currentSelect == this)
+            return;
+
+        Active();
+    }
+
+    void Active()
+    {
+        if(GameManager.instance.currentSelect != null)
+        {
+            GameManager.instance.currentSelect.activeFrame.SetActive(false);
+            GameManager.instance.currentSelect.arrow.SetActive(false);
+        }
+
+        activeFrame.SetActive(true);
+        arrow.SetActive(true);
+
+        GameManager.instance.currentSelect = this;
+        GameManager.colorSelect = colorSelect;
+    }
+}
